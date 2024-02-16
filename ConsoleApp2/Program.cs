22f53e0e496b5b4d8c64a26f834035b642283ebf
Hristian2314 <f75@abv.bg>
2024-02-16 13:18:10 +0200

    new project

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int midle = number.Length / 2;
+
+            if (number.Length == 1)
+            {
+                Console.WriteLine(number[0]);
+
+            }
+            else if (number.Length % 2 == 0)
+            {
+                Console.WriteLine(number[midle - 1]);
+                Console.WriteLine(number[midle]);
+            }
+            else
+            {
+                Console.WriteLine(number[midle - 1]);
+                Console.WriteLine(number[midle]);
+                Console.WriteLine(number[midle - 1]);
+            }
+        }
+    }
+}
